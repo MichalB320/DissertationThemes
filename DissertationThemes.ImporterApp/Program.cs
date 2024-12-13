5ec9857df7daa37efacc1310f8108e45c40a7602
Michal Bežo <miso.bezo@centrum.sk>
2024-12-13 17:40:32 +0100

    try

--- DissertationThemes.ImporterApp/Program.cs
+++ DissertationThemes.ImporterApp/Program.cs
@@ -7,55 +7,59 @@ internal class Program
 {
     public static async Task Main(string[] args)
     {
-        IS importer = new();
+        try
+        {
+            IS importer = new();
 
-        var rootCommand = new RootCommand
+            var rootCommand = new RootCommand
             {
-                new Argument<string>("argument", "path to the file.") { Arity = ArgumentArity.ZeroOrOne }, 
+                new Argument<string>("argument", "path to the file.") { Arity = ArgumentArity.ZeroOrOne },
                 new Option<bool>("-r", "remove previous data"),
                 new Option<bool>("--remove-previous-data", "remove previous data"),
             };
 
-        rootCommand.Description = "importer app for phd themes";
+            rootCommand.Description = "importer app for phd themes";
 
-        rootCommand.Handler = CommandHandler.Create<string, bool, bool>(async (argument, r, removePreviousData) =>
-        {
-            if (string.IsNullOrEmpty(argument))
+            rootCommand.Handler = CommandHandler.Create<string, bool, bool>(async (argument, r, removePreviousData) =>
             {
-                if (r || removePreviousData)
+                if (string.IsNullOrEmpty(argument))
                 {
-                    Console.Write("Zadaj cestu k súboru: ");
-                    argument = Console.ReadLine();
+                    if (r || removePreviousData)
+                    {
+                        Console.Write("Zadaj cestu k suboru: ");
+                        argument = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Neboli zadane ziadne argumenty ani volitelne flagy. Ukoncujem program.");
+                        return;
+                    }
+                }
+
+                if (r)
+                {
+                    Console.WriteLine($"Flag -r pouzity s argumentom: {argument}");
+                    importer.Delete();
+                }
+                else if (removePreviousData)
+                {
+                    Console.WriteLine($"Flag --remove-previous-data pouzity s argumentom: {argument}");
+                    importer.Delete();
                 }
                 else
                 {
-                    Console.WriteLine("Neboli zadané žiadne argumenty ani voliteľné flagy. Ukončujem program.");
-                    return;
+                    Console.WriteLine($"Spracovavam subor na ceste: {argument}");
+                    importer.ReadData(argument);
                 }
-            }
+            });
 
-            if (r)
-            {
-                Console.WriteLine($"Flag -r použitý s argumentom: {argument}");
-                importer.Delete();
-                // Logika pre mazanie dát
-            }
-            else if (removePreviousData)
-            {
-                Console.WriteLine($"Flag --remove-previous-data použitý s argumentom: {argument}");
-                importer.Delete();
-                // Logika pre mazanie dát
-            }
-            else
-            {
-                Console.WriteLine($"Spracovávam súbor na ceste: {argument}");
-                importer.ReadData(argument);
-                // Logika pre spracovanie dát zo súboru
-            }
-        });
+            await rootCommand.InvokeAsync(args);
 
-        await rootCommand.InvokeAsync(args);
-
-        importer.ShowAll();
+            //importer.ShowAll();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
     }
 }
